f63adee87a5d89aa82ba8f3dbcda37a4032b3caa
blazenn2 <hamzanawab.bs@gmail.com>
2023-06-24 19:27:37 +0500

    Implemented post request addition of user

--- src/main/java/com/example/demo/user/UserResource.java
+++ src/main/java/com/example/demo/user/UserResource.java
@@ -1,9 +1,7 @@
 package com.example.demo.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +22,10 @@ public class UserResource {
     @GetMapping(path="/users/{id}")
     public User retrieveUser(@PathVariable int id) {
         return service.findOne(id);
+    }
+
+    @PostMapping(path = "/users")
+    public void createUser(@RequestBody User user) {
+        User newUser = service.save(user);
     }
 }
