bba2d6bc1cd5a9c42647de64dd151bf298fa8401
blazenn2 <hamzanawab.bs@gmail.com>
2023-06-25 19:14:28 +0500

    Exception handling of invalid request

--- src/main/java/com/example/demo/user/UserResource.java
+++ src/main/java/com/example/demo/user/UserResource.java
@@ -1,5 +1,6 @@
 package com.example.demo.user;
 
+import com.example.demo.exceptions.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,7 +25,9 @@ public class UserResource {
 
     @GetMapping(path="/users/{id}")
     public User retrieveUser(@PathVariable int id) {
-        return service.findOne(id);
+        User user = service.findOne(id);
+        if (user == null) throw new UserNotFoundException("id-" + id);
+        else return user;
     }
 
     @PostMapping(path = "/users")
